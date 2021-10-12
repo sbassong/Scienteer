4b0dd942f2bc7cd34c8d6190d9b4a9c69ae6401d
Sam Bassong <sam.bassong@gmail.com>
2021-10-12 09:12:18 -0700

    set up projects services

---
+++ client/src/services/project.js
@@ -0,0 +1,35 @@
+import Client from './api'
+
+
+
+export const GetAllProjects = async () => {
+    const res = await Client.post(`projects`)
+    return res.data
+}
+
+export const CreateProject = async (data) => {
+    const res = await Client.post(`projects`, data)
+    return res.data
+}
+
+export const UpdateProject = async (project_id, data) => {
+    const res = await Client.put(`project/${project_id}`, data)
+    return res.data
+}
+
+export const DeleteProject = async (project_id) => {
+    const res = await Client.put(`project/${project_id}`)
+    return res.data
+}
+
+export const GetProjectByCategory = async (category) => {
+    const res = await Client.get(`projects/category/${category}`)
+    return res.data
+}
+
+export const GetProjectByUserId = async (user_id) => {
+    const res = await Client.get(`projects/researcher/${user_id}`)
+    return res.data
+}
+
+
