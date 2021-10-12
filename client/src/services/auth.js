28266e39a74a474eb1b4ccdf2f45c6fa6f3b423e
Sam Bassong <sam.bassong@gmail.com>
2021-10-12 08:52:10 -0700

    set up auth services

---
+++ client/src/services/auth.js
@@ -0,0 +1,17 @@
+import Client from './api'
+
+export const LoginUser = async (data) => {
+    const res = await Client.post('auth/login', data)
+    localStorage.setItem('token', res.data.token)
+    return res.data.user
+}
+
+export const RegisterUser = async (data) => {
+    const res = await Client.post('auth/register', data)
+    return res.data
+}
+
+export const CheckSession = async () => {
+    const res = await Client.get('auth/login')
+    return res.data
+}
