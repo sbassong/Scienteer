51d9efbeeeba6581ceb358435ce59759128877e8
Sam Bassong <sam.bassong@gmail.com>
2021-10-12 08:50:17 -0700

    installed vuex and set up base store in main.js

--- client/src/main.js
+++ client/src/main.js
@@ -1,8 +1,34 @@
 import Vue from 'vue'
 import App from './App.vue'
+import VueRouter from 'vue-router'
+import router from './router'
+import Vuex from 'vuex'
+
 
 Vue.config.productionTip = false
 
+Vue.use(VueRouter)
+Vue.use(Vuex)
+
+const store = new Vuex.Store({
+  state: {
+    user: {},
+    projects: [],
+    reports: []
+  },
+  mutations: {
+    // increment (state) {
+    //   state.count++
+    // }
+  }
+})
+
+
 new Vue({
   render: h => h(App),
+  router,
+  store
 }).$mount('#app')
+
+
+
---
+++ client/src/router.js
@@ -0,0 +1,20 @@
+import VueRouter from 'vue-router'
+
+import ArtistsList from './pages/ArtistsList'
+import ArtistDetails from './pages/ArtistDetails'
+import About from './pages/About'
+import Home from './pages/Home'
+import LoginForm from './components/LoginForm'
+import RegisterForm from './components/RegisterForm'
+
+
+const routes = [
+  {path: '/', component: Home, name: 'Home'},
+  {path: '/artists', component: ArtistsList, name: 'ArtistsList'},
+  {path: '/artists/details/:artist_id', component: ArtistDetails, name: 'ArtistDetails'},
+  {path: '/about', component: About, name: 'About'},
+  {path: '/users/login', component: LoginForm, name: 'LoginForm'},
+  {path: '/users/register', component: RegisterForm, name: 'RegisterForm'}
+]
+
+export default new VueRouter({ routes, mode: 'history' })
