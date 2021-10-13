5ae2c2e17d2b3abd6c71bf14623a864d08584d14
Sam Bassong <sam.bassong@gmail.com>
2021-10-13 08:52:07 -0700

    set UpdateProfileForm and func

--- client/src/components/UpdateProfileForm.vue
+++ client/src/components/UpdateProfileForm.vue
@@ -1,37 +1,41 @@
 <template>
   <form >
-    
-    <v-text-field v-model="old_password" label="Password" required placeholder="Enter password here" name='password' @change="$v.password.$touch()" @blur="$v.password.$touch()"></v-text-field>
-    <v-text-field v-model="old_password" label="Password" required placeholder="Enter password here" name='password' @change="$v.password.$touch()" @blur="$v.password.$touch()"></v-text-field>
-    <v-text-field v-model="password" label="Password" required placeholder="Enter password here" name='password' @change="$v.password.$touch()" @blur="$v.password.$touch()"></v-text-field>
-    <v-btn class="mr-4" @click='handleSubmit'>Update Password</v-btn>
+    <v-text-field v-model="name" label="Name" @change="$v.name.$touch()" @blur="$v.name.$touch()"></v-text-field>
+    <v-text-field v-model="email" label="Email" @change="$v.email.$touch()" @blur="$v.email.$touch()"></v-text-field>
+    <v-text-field v-model="bio" label="Bio" @change="$v.bio.$touch()" @blur="$v.bio.$touch()"></v-text-field>
+    <!-- <v-text-field v-model="image" label="Image" @change="$v.image.$touch()" @blur="$v.image.$touch()"></v-text-field> -->
+    <v-btn class="mr-4" @click='handleSubmit'>Update Profile</v-btn>
   </form>
 </template>
 
 <script>
-import {UpdateUserPW} from '../services/user'
+import {UpdateProfile} from '../services/user'
 
 export default {
-  name: 'UpdatePasswordForm',
+  name: 'UpdateProfileForm',
 
   data: () => ({
-    old_password: '',
-    new_password: '',
-    c_new_password: '',
-    current_user: null, //session user
+    name: current_user.name || null,
+    email: current_user.email || null,
+    bio: current_user.bio || null,
+    // image: current_user.image || null', future addition to user table
+    current_user: null //session user
   }),
 
   methods: {
     async handleSubmit(event) {
       event.preventDefault()
-      const passwordBody = {
-        old_password: this.old_password,
-        new_password: this.new_password
+      const profileBody = {
+        name: this.name,
+        email: this.email,
+        bio: this.bio,
+        // image: this.image 
       }
-      await UpdateUserPW(passwordBody)
-      this.old_password = ''
-      this.new_password = ''
-      this.c_new_password = ''
+      await UpdateProfile(current_user.id, profileBody)
+      this.name = current_user.name || null
+      this.email = current_user.email || null
+      this.bio = current_user.bio || null
+      // this.image = current_user.image || null
       this.$router.push('/users/profile')
     }
   },
